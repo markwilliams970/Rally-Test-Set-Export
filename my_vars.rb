319e6bb4b761e5629411b7304db8488ea65e7a6c
U-cirque\markwilliams <markwilliams@cirque.(none)>
2013-06-17 10:48:09 -0600

    User-defined environment variables

---
+++ my_vars.rb
@@ -0,0 +1,18 @@
+# Connection Parameters
+$my_base_url              = "https://rally1.rallydev.com/slm"
+$my_username              = "user@company.com"
+$my_password              = "topsecret"
+$my_page_size             = 200
+$my_limit                 = 50000
+$my_delim                 = "\t"
+$wsapi_version            = "1.43"
+
+# Workspace/project info
+$my_workspace             = "My Workspace"
+$my_project               = "My Project"
+
+# Test Set Info
+$my_test_set_formatted_id = "TS7"
+
+# output
+$my_output_file           = "testset.txt"
